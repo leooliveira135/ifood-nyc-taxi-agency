c4d26088599e923347f4072d7208114dc7186317
Leonardo Oliveira <oliveiraleonardo61@gmail.com>
2026-01-15 07:59:16 -0300

    Refactoring the main.py file

--- src/ifood/etl/glue_setup.py
+++ src/ifood/etl/glue_setup.py
@@ -1,7 +1,7 @@
 import logging
 from ifood.aws.glue_catalog import create_glue_database, create_glue_crawler, start_glue_crawler, list_glue_db_tables, create_glue_job, run_glue_job
 from ifood.aws.s3_bucket import upload_file_s3_bucket
-from ifood.vars import glue_database, glue_database_stg, s3_stg_bucket, s3_bucket, glue_iceberg_job, glue_job_path, iceberg_bucket, glue_iceberg_job_path
+from ifood.vars import glue_database, glue_database_stg, s3_stg_bucket, glue_iceberg_job, glue_job_path, iceberg_bucket, glue_iceberg_job_path
 
 def glue_setup(aws_region: str, account_id: str) -> None:
     """
@@ -15,22 +15,14 @@ def glue_setup(aws_region: str, account_id: str) -> None:
     logging.info("Setting up AWS Glue database...")
     logging.info(f"Creating Glue crawler for staging database {glue_database_stg} in {aws_region}...")
     create_glue_database(glue_database_stg, aws_region)
-    logging.info(f"Creating Glue crawler for production database {glue_database} in {aws_region}...")
-    create_glue_database(glue_database, aws_region)    
     logging.info("AWS Glue databases created successfully.")
     crawler_name_stg = f"{glue_database_stg}_crawler"
-    crawler_name = f"{glue_database}_crawler"
     s3_path_stg = f"s3://{s3_stg_bucket}/"
     logging.info("Creating AWS Glue crawlers...")
     create_glue_crawler(crawler_name_stg, glue_database_stg, s3_path_stg, aws_region, account_id)
-    table_list = list_glue_db_tables(glue_database_stg)
-    for table in table_list:
-        s3_path = f"s3://{s3_bucket}/{table}"
-        create_glue_crawler(crawler_name, glue_database, s3_path, aws_region, account_id)
     logging.info("AWS Glue crawlers created successfully.")
     logging.info("Starting AWS Glue crawlers...")
-    start_glue_crawler(crawler_name_stg, glue_database_stg)
-    start_glue_crawler(crawler_name, glue_database)
+    start_glue_crawler(crawler_name_stg)
     logging.info("AWS Glue crawlers started and completed successfully.")
 
 def iceberg_setup(aws_region: str, account_id: str) -> None:
@@ -48,15 +40,12 @@ def iceberg_setup(aws_region: str, account_id: str) -> None:
     logging.info(f"Uploading {glue_iceberg_job_path} file into {bucket_name}")
     upload_file_s3_bucket(bucket_name, bucket_key, glue_iceberg_job_path)
     logging.info(f"Creating Glue Job for Iceberg tables {glue_iceberg_job}")
-    s3_path = f"s3://{glue_job_path}/"
-    create_glue_job(glue_iceberg_job, account_id, aws_region, s3_path)
+    create_glue_job(glue_iceberg_job, account_id, aws_region, glue_iceberg_job_path)
     logging.info("AWS Glue Job created successfully.")
     table_list = list_glue_db_tables(glue_database)
     for table in table_list:
-        source_path = f"s3://{s3_bucket}/{table}"
-        iceberg_path = f"s3://{iceberg_bucket}/{table}"
         logging.info(f"Starting AWS Glue Job for the Iceberg table {table}...")
-        job_run_id = run_glue_job(glue_iceberg_job, glue_database, source_path, glue_database, table, iceberg_path)
+        job_run_id = run_glue_job(glue_iceberg_job, table, glue_database_stg, glue_database, iceberg_bucket)
         logging.info(f"AWS Glue Job for the Iceberg table {table} completed with status {job_run_id}...")
 
 
